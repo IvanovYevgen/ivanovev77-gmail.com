767acde528d2161e62c57455e0bbd9a83e611a9d
--global <johndoe@example.com>
2020-04-04 22:39:47 +0300

    first commit

---
+++ src/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../About/About.css';
+
+
+class About extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+
+      <div>
+        lorem20
+      </div>
+
+    );
+  }
+}
+
+export default About;
